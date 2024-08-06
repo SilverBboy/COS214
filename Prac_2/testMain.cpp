c2228f58d432985ef50f3e8eccf95ac8aa042a3d
Daniel@Dell7720 <severyan100@gmail.com>
2024-08-06 11:11:00 +0200

    Small changes

---
+++ Prac_2/testMain.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include "Soldiers.h"
+#include "SoldierFactory.h"
+#include "Infantry.h"
+#include "InfantryFactory.h"
+#include "Game.h"
+
+int main(){
+    try{
+        Game game;
+        game.start();
+        return 0;
+    }
+    catch(char const* message){
+        std::cout<<message<<std::endl;
+    }
+}
